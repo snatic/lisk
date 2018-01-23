585d7da4c8b47d03234212008b0be3f88c80d23d
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-01-23 17:29:31 +0000

    Transport tests - receiveTransaction method; check that library.balancesSequence.add gets called internally

--- test/unit/modules/transport.js
+++ test/unit/modules/transport.js
@@ -711,7 +711,12 @@ peerAddressString;
 				});
 			});
 
-			it('should call library.balancesSequence.add');
+			it('should call library.balancesSequence.add', function (done) {
+				__private.receiveTransaction(transaction, peerStub, 'This is a log message', function (err) {
+					expect(library.balancesSequence.add.called).to.be.true;
+					done();
+				});
+			});
 
 			describe('when peer is undefined', () => {
 				it('should call library.logger.debug with "Received transaction " + transaction.id + " from public client"');
